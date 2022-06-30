27662f9c7f73403a312d14c88533271aecd496b5
pavsta2 <pavelstarshinov@gmail.com>
2022-06-30 21:40:00 +0300

    end practice 30/06

--- Note_client_back.py
+++ Note_client_back.py
@@ -14,28 +14,47 @@ class Note_Form(QtWidgets.QMainWindow):
         self.ui = Form()
         self.ui.setupUi(self)
 
+        self.login = None
+        self.password = None
+
         self.initUi()
+        self.initWindows()
 
     def initUi(self):
+        self.ui.comboBox_importance.addItems(["False", "True"])
+        self.ui.comboBox_condition.addItems(["Активно", "Отложено", "Выполнено"])
         # сигналы
         self.ui.pushButton_get_all_notes.clicked.connect(self.getNoteList)
 
         self.ui.pushButton.clicked.connect(self.postNote)
 
+        self.ui.pushButton_login.clicked.connect(self.open_child_window)
+
+    def initWindows(self):
+        self.child_window = DroppedWindow()
+        self.child_window.send_data.connect(self.setLoginPass)
+
+    def open_child_window(self):
+        self.child_window.show()
+
     # слоты
     def getNoteList(self):
         response = requests.get("http://127.0.0.1:8000/notes/")
-        output = response.text
-        self.ui.textBrowser_window_output.setText(output)
+        output = response.json()
+        self.ui.textBrowser_window_output.setText(str(output))
 
     def postNote(self):
-        username = 'admin'
-        password = 'admin'
-        auth = HTTPBasicAuth(username, password)
+        auth = HTTPBasicAuth(self.login, self.password)
+
+        condition_index = {
+            "Активно": 1,
+            "Отложено": 2,
+            "Выполнено": 3,
+        }
 
         # data = {
         #
-        #     "title": "3000",
+        #     "title": "7000",
         #     "message": "fdgsdgsg",
         #     "importance": True,
         #     "condition": 1,
@@ -43,10 +62,54 @@ class Note_Form(QtWidgets.QMainWindow):
         #     "author": "admin"
         #      }
 
-        data = self.ui.textEdit_window_input.toPlainText()
+        data = {
+            "title": self.ui.lineEdit_title.text(),
+            "message": self.ui.lineEdit_massage.text(),
+            "importance": self.ui.comboBox_importance.currentText(),
+            "condition": condition_index[self.ui.comboBox_condition.currentText()],
+        }
+
         response = requests.post("http://127.0.0.1:8000/notes/", auth=auth, json=data)
         self.ui.textBrowser_window_output.setText(str(response.status_code))
-        print(self.ui.textEdit_window_input.toPlainText())
+
+    def setLoginPass(self, emit):
+
+        self.login = emit[0]
+        self.password = emit[1]
+
+        print(self.login, self.password)
+
+
+class DroppedWindow(QtWidgets.QWidget):
+    send_data = QtCore.Signal(tuple)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi()
+
+    def initUi(self):
+        self.setFixedSize(300, 100)
+
+        self.lineEditLogin = QtWidgets.QLineEdit()
+        self.lineEditLogin.setObjectName("Логин")
+
+        self.lineEditPass = QtWidgets.QLineEdit()
+        self.lineEditPass.setObjectName("Пароль")
+
+        pb = QtWidgets.QPushButton("LOG IN")
+        pb.clicked.connect(self.setSignal)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.lineEditLogin)
+        layout.addWidget(self.lineEditPass)
+        layout.addWidget(pb)
+
+        self.setLayout(layout)
+
+    def setSignal(self):
+        self.send_data.emit((self.lineEditLogin.text(), self.lineEditPass.text()))
+
 
 
 if __name__ == "__main__":
--- Notes_client_design_form.py
+++ Notes_client_design_form.py
@@ -17,13 +17,13 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(625, 516)
+        MainWindow.resize(719, 733)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.pushButton_get_all_notes = QPushButton(self.centralwidget)
@@ -37,7 +37,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addWidget(self.pushButton)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_3.addLayout(self.horizontalLayout)
 
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -56,21 +56,101 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.label_window_input)
 
-        self.textEdit_window_input = QTextEdit(self.centralwidget)
-        self.textEdit_window_input.setObjectName(u"textEdit_window_input")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
 
-        self.verticalLayout.addWidget(self.textEdit_window_input)
+        self.horizontalLayout_3.addWidget(self.label)
+
+        self.lineEdit_title = QLineEdit(self.centralwidget)
+        self.lineEdit_title.setObjectName(u"lineEdit_title")
+
+        self.horizontalLayout_3.addWidget(self.lineEdit_title)
 
 
-        self.verticalLayout_2.addLayout(self.verticalLayout)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_4.addWidget(self.label_2)
+
+        self.lineEdit_massage = QLineEdit(self.centralwidget)
+        self.lineEdit_massage.setObjectName(u"lineEdit_massage")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit_massage)
 
 
-        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout_5.addWidget(self.label_3)
+
+        self.comboBox_importance = QComboBox(self.centralwidget)
+        self.comboBox_importance.setObjectName(u"comboBox_importance")
+
+        self.horizontalLayout_5.addWidget(self.comboBox_importance)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_5)
+
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_6.addWidget(self.label_4)
+
+        self.comboBox_condition = QComboBox(self.centralwidget)
+        self.comboBox_condition.setObjectName(u"comboBox_condition")
+
+        self.horizontalLayout_6.addWidget(self.comboBox_condition)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
+
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_7)
+
+        self.horizontalLayout_8 = QHBoxLayout()
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_8.addWidget(self.label_6)
+
+        self.pushButton_login = QPushButton(self.centralwidget)
+        self.pushButton_login.setObjectName(u"pushButton_login")
+
+        self.horizontalLayout_8.addWidget(self.pushButton_login)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_8)
+
+
+        self.verticalLayout.addLayout(self.verticalLayout_2)
+
+
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_4.addLayout(self.verticalLayout_3)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 625, 22))
+        self.menubar.setGeometry(QRect(0, 0, 719, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -86,6 +166,12 @@ class Ui_MainWindow(object):
         self.pushButton_get_all_notes.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0441\u0435 \u0437\u0430\u043f\u0438\u0441\u0438", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041e\u043f\u0443\u0431\u043b\u0438\u043a\u043e\u0432\u0430\u0442\u044c", None))
         self.labe_window_output.setText(QCoreApplication.translate("MainWindow", u"\u041e\u043a\u043d\u043e \u0432\u044b\u0432\u043e\u0434\u0430", None))
-        self.label_window_input.setText(QCoreApplication.translate("MainWindow", u"\u041e\u043a\u043d\u043e \u0432\u0432\u043e\u0434\u0430", None))
+        self.label_window_input.setText(QCoreApplication.translate("MainWindow", u"\u041e\u043f\u0443\u0431\u043b\u0438\u043a\u043e\u0432\u0430\u0442\u044c \u0437\u0430\u043f\u0438\u0441\u044c:", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\"title\":", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\"message\":", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\"importance\":", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\"condition\":", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043b\u044f \u043f\u0443\u0431\u043b\u0438\u0430\u0446\u0438\u0438 \u0430\u0432\u0442\u043e\u0440\u0438\u0437\u0443\u0439\u0442\u0435\u0441\u044c:", None))
+        self.pushButton_login.setText(QCoreApplication.translate("MainWindow", u"LOG IN", None))
     # retranslateUi
 
